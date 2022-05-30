159e7ded50c16239aeffff7249a5bc2891db5bc1
delasimensah <mensadelasi@gmail.com>
2022-05-30 16:12:18 +0000

    added context for app

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ContextProvider from "./contexts/ContextProvider";
+
 import App from "./App";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>
 );
